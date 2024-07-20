d4b1b62cea834ef6ffd2cda714cec6d0bbf54e9e
Ariel Spencer <arielspencer.tech@gmail.com>
2024-07-20 14:16:21 -0300

    Refatorar link whatsapp

--- components/Header.jsx
+++ components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
       {/* desktop nav */}
       <div className="hidden xl:flex items-center gap-8">
         <Nav />
-        <Link href="http://wa.me/5511991007079">
+        <Link href="https://wa.me/5511991007079">
           {/* <Button className="capitalize font-medium hover:text-accent translation-all">Olá!</Button> */}
           <Button>Olá!</Button>
         </Link>
