8c3ae2f24a794961210aa20a338f077a8867ac39
Ariel Spencer <arielspencer.tech@gmail.com>
2024-07-21 23:55:31 -0300

    Alterar cor das escadas de transição

--- app/sobre-mim/page.jsx
+++ app/sobre-mim/page.jsx
@@ -214,10 +214,10 @@ const About = () => {
                 <div className="max-w-[620px] text-white/60 mx-auto xl:mx-0">
                   {resume.description.map((item, index) => {
                     return (
-                      <TooltipProvider delayDuration={100}>
+                      <TooltipProvider key={index} delayDuration={100}>
                         <Tooltip>
                           <TooltipTrigger>
-                            <ScrollArea key={item.index} className="h-[250px] w-full text-center xl:text-left">
+                            <ScrollArea className="h-[250px] w-full text-center xl:text-left">
                               <div className="max-w-[98%]">
                                 <p>
                                   {item.primary}
@@ -243,7 +243,7 @@ const About = () => {
                 <ul className="grid grid-cols-1 md:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0">
                   {resume.info.map((item, index) => {
                     return (
-                      <li key={resume.index} className="flex items-center justify-center xl:justify-start gap-3">
+                      <li key={index} className="flex items-center justify-center xl:justify-start gap-3">
                         <span className="text-white/60">{item.fieldName}</span>
                         <a href={item.href} className={`${item.href != "" ? 'hover:text-accent' : 'pointer-events-none cursor-default'}`}>{item.fieldValue}</a>
                       </li>
@@ -269,7 +269,7 @@ const About = () => {
                   <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 xl:gap-[30px]">
                     {skills.skillList.map((skill, index) => {
                       return (
-                        <li key={skills.index}>
+                        <li key={index}>
                           <TooltipProvider delayDuration={100}>
                             <Tooltip>
 
@@ -310,7 +310,7 @@ const About = () => {
                     {education.items.map((item, index) => {
                       return (
                         <li
-                          key={education.index}
+                          key={index}
                           className="bg-[#232329] h-[235px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
                         >
                           <span className="text-accent">
@@ -349,7 +349,7 @@ const About = () => {
                     {experience.items.map((item, index) => {
                       return (
                         <li
-                          key={experience.index}
+                          key={index}
                           className="bg-[#232329] h-[235px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
                         >
                           <span className="text-accent">
