090306a1ea93454426ba13401e07fafc71a2d4af
Ariel Spencer <arielspencer.tech@gmail.com>
2024-08-31 18:56:08 -0300

    chore(header): remove comments

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,23 +7,19 @@ import MobileNav from "./MobileNav";
 const Header = () => {
   return <header className="py-8 xl:py-12 text-white">
     <div className="container mx-auto flex justify-between items-center">
-      {/* logo here */}
       <Link href="/">
         <h1 className="text-4xl font-semibold">
           Ariel <span className="text-accent">Spencer</span>
         </h1>
       </Link>
 
-      {/* desktop nav */}
       <div className="hidden xl:flex items-center gap-8">
         <Nav />
         <Link href="https://wa.me/5511991007079">
-          {/* <Button className="capitalize font-medium hover:text-accent translation-all">Olá!</Button> */}
           <Button>Olá!</Button>
         </Link>
       </div>
 
-      {/* mobile nav */}
       <div className="xl:hidden">
         <MobileNav />
       </div>
