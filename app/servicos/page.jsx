e9ee552d794b09a25bebdb54c4b07770cc7f4cc1
Ariel Spencer <arielspencer.tech@gmail.com>
2024-09-01 19:27:53 -0300

    feat(services): set dark mode

--- app/servicos/page.jsx
+++ app/servicos/page.jsx
@@ -51,21 +51,25 @@ const Services = () => {
                 className="flex-1 flex flex-col justify-center gap-6 group"
               >
                 <div className="w-full flex justify-between items-center">
-                  <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">
-                    {service.num}
-                  </div>
+                  <Link href={service.href}>
+                    <div className="text-5xl font-extrabold text-outline dark:text-dark-outline text-transparent group-hover:text-outline-hover dark:group-hover:text-dark-outline-hover transition-all duration-500">
+                      {service.num}
+                    </div>
+                  </Link>
                   <Link
                     href={service.href}
-                    className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45"
+                    className="w-[70px] h-[70px] rounded-full bg-writing-rel dark:bg-accent-dark group-hover:bg-accent dark:group-hover:bg-accent-darkhover transition-all duration-500 flex justify-center items-center group-hover:-rotate-45"
                   >
                     <BsArrowDownRight
-                      className="text-primary text-3xl"
+                      className="text-primary group-hover:text-writing-dark dark:group-hover:text-writing text-3xl"
                     />
                   </Link>
                 </div>
 
-                <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">{service.title}</h2>
-                <p className="text-white/60">{service.description}</p>
+                <Link href={service.href}>
+                  <h2 className="text-[42px] font-bold leading-none text-accent-dark group-hover:text-accent dark:group-hover:text-writing-rel transition-all duration-500">{service.title}</h2>
+                </Link>
+                <p className="text-writing/60 dark:text-writing-dark">{service.description}</p>
 
                 <div
                   className="border-b border-white/20 w-full">
